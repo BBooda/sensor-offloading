7f5604ee0089b9434a41d83a59da9dce18d96ba8
BBooda <makis.damigos@gmail.com>
2022-07-04 13:45:56 +0000

    modify shell script to also launch interface statistics at the same time

--- edge/shell-scripts/spot_launch_sensor_stat.sh
+++ edge/shell-scripts/spot_launch_sensor_stat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# take as a command line argument the suffix of the matlab files
+
 SESSION=statistics
 
 TEMP_DIR=pwd
@@ -20,7 +22,7 @@ tmux select-pane -t 0
 
 
 # dsl, octomap, velodyne, fronters, path_
-# tmux send-keys -t 0 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /spot/vectornav/IMU stat_imu_node imu_$1" C-m
+tmux send-keys -t 0 "sleep 1; cd $TEMP_DIR; python3 ../../monitor_nw_interface.py interface_statistics_$1" C-m
 # tmux send-keys -t 1 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /spot/velodyne_points velo_points_node velo_points_$1" C-m
 # tmux send-keys -t 2 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /spot/velodyne_packets velo_packets_node velo_packets_$1" C-m
 tmux send-keys -t 2 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /camera/color/image_raw image_raw_node image_raw_$1" C-m
