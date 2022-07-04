36a1a0ab6548554e285359fa4a303c0468ec8e32
Gerasimos Damigos <gerasimos.damigos@ericsson.com>
2022-07-04 17:21:14 +0200

    configure for image raw tests

--- edge/shell-scripts/spot_launch_sensor_stat.sh
+++ edge/shell-scripts/spot_launch_sensor_stat.sh
@@ -5,6 +5,7 @@
 SESSION=statistics
 
 TEMP_DIR=pwd
+EDGE_NODE="edge_node"
 
 export ROS_IPV6=on
 export ROS_MASTER_URI=http://master:11311
@@ -22,11 +23,11 @@ tmux select-pane -t 0
 
 
 # dsl, octomap, velodyne, fronters, path_
-tmux send-keys -t 0 "sleep 1; cd $TEMP_DIR; python3 ../../monitor_nw_interface.py interface_statistics_$1" C-m
-# tmux send-keys -t 1 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /spot/velodyne_points velo_points_node velo_points_$1" C-m
-# tmux send-keys -t 2 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /spot/velodyne_packets velo_packets_node velo_packets_$1" C-m
-tmux send-keys -t 2 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /camera/color/image_raw image_raw_node image_raw_$1" C-m
-tmux send-keys -t 3 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /camera/color/image_raw/compressed image_raw_comp_node image_raw_comp_$1" C-m
+tmux send-keys -t 0 "sleep 1; cd $TEMP_DIR; python3 ../../monitor_nw_interface.py interface_statistics_$1 tx" C-m
+tmux send-keys -t 1 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /camera/color/image_raw image_raw_$EDGE_NODE image_raw_$1" C-m
+# tmux send-keys -t 2 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /camera/depth/color/points color_points_$EDGE_NODE color_points_$1" C-m
+#tmux send-keys -t 2 "sleep 10; source ~/spot_env/bin/activate; cd ~/ros_workspaces/driver_spot_ws/; source devel/setup.bash; roslaunch spot_driver driver.launch" C-m
+#tmux send-keys -t 3 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /camera/color/image_raw/compressed image_comp_$EDGE_NODE image_comp_$1" C-m
 #tmux send-keys -t 4 "sleep 20; cd ~/catkin_ws/spot_ws/; source devel/setup.bash; roslaunch vectornav vectornav.launch" C-m
 
 tmux -2 attach-session -t $SESSION
--- spot/shell-scripts/launch_log_statistics.sh
+++ spot/shell-scripts/launch_log_statistics.sh
@@ -2,7 +2,8 @@
 
 SESSION=statistics
 
-TEMP_DIR=/home/spotnuc/temp
+TEMP_DIR=pwd
+SPOT_NODE="spot_node"
 
 export ROS_IPV6=on
 export ROS_MASTER_URI=http://master:11311
@@ -20,11 +21,11 @@ tmux select-pane -t 0
 
 
 # dsl, octomap, velodyne, fronters, path_
-tmux send-keys -t 0 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /spot/vectornav/IMU stat_imu_node imu_$1" C-m
-tmux send-keys -t 1 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /spot/velodyne_points velo_points_node velo_points_$1" C-m
-tmux send-keys -t 2 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /spot/velodyne_packets velo_packets_node velo_packets_$1" C-m
+tmux send-keys -t 0 "sleep 1; cd $TEMP_DIR; python3 ../../monitor_nw_interface.py interface_statistics_$1 tx" C-m
+# tmux send-keys -t 1 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /camera/color/image_raw image_raw_$SPOT_NODE image_raw_$1" C-m
+# tmux send-keys -t 2 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /camera/depth/color/points color_points_$SPOT_NODE color_points_$1" C-m
 #tmux send-keys -t 2 "sleep 10; source ~/spot_env/bin/activate; cd ~/ros_workspaces/driver_spot_ws/; source devel/setup.bash; roslaunch spot_driver driver.launch" C-m
-#tmux send-keys -t 3 "sleep 16; cd ~/catkin_ws/spot_ws/; source devel/setup.bash; roslaunch velodyne_pointcloud VLP16_points.launch" C-m
+#tmux send-keys -t 3 "sleep 5; cd $TEMP_DIR; python3 ../log_statistics_v1.py /camera/color/image_raw/compressed image_comp_$SPOT_NODE image_comp_$1" C-m
 #tmux send-keys -t 4 "sleep 20; cd ~/catkin_ws/spot_ws/; source devel/setup.bash; roslaunch vectornav vectornav.launch" C-m
 
 tmux -2 attach-session -t $SESSION
