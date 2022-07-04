2887502cc7d027c260fb347a8d44866a47decd0d
Gerasimos Damigos <gerasimos.damigos@ericsson.com>
2022-07-04 12:28:39 +0200

    python script to monitor network interface, might need a small modification for viewing the result. Reads from linux file rx or tx and outputs the result

---
+++ monitor_nw_interface.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import time
+
+def transmissionrate(dev, direction, timestep):
+    """Return the transmisson rate of a interface under linux
+    dev: devicename
+    direction: rx (received) or tx (sended)
+    timestep: time to measure in seconds
+    """
+    path = "/sys/class/net/{}/statistics/{}_bytes".format(dev, direction)
+    f = open(path, "r")
+    bytes_before = int(f.read())
+    f.close()
+    time.sleep(timestep)
+    f = open(path, "r")
+    bytes_after = int(f.read())
+    f.close()
+    return (bytes_after-bytes_before)/timestep
+
+
+if __name__ == "__main__":
+    counter = 1
+    devname = "hnet0"
+    timestep = 1 # Seconds
+    
+    while True:
+        print(transmissionrate(devname, "rx", timestep))
+        counter += 1
+        time.sleep(1)
