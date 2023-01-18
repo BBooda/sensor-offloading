3af0ea69dc1553fd99c852f03b65c29a3ce00ece
makis.damigos@gmail.com <nstathou@gmail.com>
2023-01-18 21:02:52 +0000

    working settings with b4 onboard pc and vm edge as the remote agent-note tested yet

---
+++ udp_tunnel_tests/start_tunnel_edge.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# take as a command line argument the suffix of the matlab files
+
+SESSION=tunnel_edge
+
+TEMP_DIR=$(pwd)
+
+SERVINING_IP="127.0.0.1"
+TARGET_IP="130.240.118.133"
+
+tmux -2 new-session -d -s $SESSION
+
+# tmux setenv ROS_IP 127.0.0.1
+
+tmux split-window -h
+tmux select-pane -t 0
+tmux split-window -v
+tmux select-pane -t 2
+tmux split-window -v
+# tmux select-pane -t 0
+# tmux split-window -v
+# tmux select-pane -t 0
+
+# NOTES:
+# For the server script, First argument: topic_to_publish, 
+#                        Second argument: publishing rate
+#                        thrird argument: node name
+#                        Forth argument: Serving IP
+#                        Fifth argument: Port
+tmux send-keys -t 1 "sleep 1; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_server_py.py /udp_odom 100 odometry $SERVINING_IP 30001" C-m
+
+# NOTES:
+# For the client script, First argument: topic_to_subscribe, 
+#                        Second argument: node name
+#                        Third argument: IP to connect to
+#                        Forth argument: Port
+
+tmux send-keys -t 0 "sleep 2; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_sub_client.py /cmd_velocities velocities $TARGET_IP 30002" C-m
+
+# for reading the latencies
+# tmux send-keys -t 1 "sleep 1; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_server_py.py /latencies 40 latencies $SERVINING_IP 30003" C-m
+
+
+
+
+tmux -2 attach-session -t $SESSION
---
+++ udp_tunnel_tests/start_tunnel_onboard.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# take as a command line argument the suffix of the matlab files
+
+SESSION=tunnel
+
+TEMP_DIR=$(pwd)
+
+TARGET_IP="130.240.22.40"
+SERVING_IP="192.168.0.52"
+
+tmux -2 new-session -d -s $SESSION
+
+# tmux setenv ROS_IP 127.0.0.1
+
+tmux split-window -h
+tmux select-pane -t 0
+tmux split-window -v
+tmux select-pane -t 1
+tmux split-window -v
+# tmux select-pane -t 0
+# tmux split-window -v
+# tmux select-pane -t 0
+
+
+# NOTES:
+# For the client script, First argument: topic_to_subscribe, 
+#                        Second argument: node name
+#                        Third argument: IP to connect to
+#                        Forth argument: Port
+tmux send-keys -t 1 "sleep 2; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_sub_client.py /pixy/vicon/demo_crazyflie6/demo_crazyflie6/odom odometry $TARGET_IP 31404" C-m
+
+# NOTES:
+# For the server script, First argument: topic_to_publish, 
+#                        Second argument: publishing rate
+#                        thrird argument: node name
+#                        Forth argument: Serving IP
+#                        Fifth argument: Port
+
+tmux send-keys -t 2 "sleep 1; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_server_twist_py.py /demo_crazyflie12/cmd_vel 30 cmd_vel $SERVING_IP 31349" C-m
+
+# capture and send the latencies
+tmux send-keys -t 3 "sleep 2; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_sub_client_twist.py /demo_crazyflie12/cmd_vel late $TARGET_IP 31182" C-m
+
+# tmux send-keys -t 1 "sleep 1; cd $TEMP_DIR; python3 udp_server_py.py" C-m
+
+
+tmux -2 attach-session -t $SESSION
