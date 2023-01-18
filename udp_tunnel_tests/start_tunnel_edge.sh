f24bbbd4b47df72c75dc7d74918c9361d64008b8
BBooda <makis.damigos@gmail.com>
2023-01-18 21:04:50 +0000

    working example for edge_vm as remote pc and b4 onboard -- not tested yet

--- udp_tunnel_tests/start_tunnel_edge.sh
+++ udp_tunnel_tests/start_tunnel_edge.sh
@@ -6,7 +6,7 @@ SESSION=tunnel_edge
 
 TEMP_DIR=$(pwd)
 
-SERVINING_IP="10.42.1.210"
+SERVINING_IP="130.240.22.40"
 TARGET_IP="130.240.118.133"
 
 tmux -2 new-session -d -s $SESSION
@@ -16,7 +16,7 @@ tmux -2 new-session -d -s $SESSION
 tmux split-window -h
 tmux select-pane -t 0
 tmux split-window -v
-tmux select-pane -t 2
+tmux select-pane -t 1
 tmux split-window -v
 # tmux select-pane -t 0
 # tmux split-window -v
@@ -28,7 +28,7 @@ tmux split-window -v
 #                        thrird argument: node name
 #                        Forth argument: Serving IP
 #                        Fifth argument: Port
-tmux send-keys -t 1 "sleep 1; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_server_py.py /pixy/vicon/demo_crazyflie12/demo_crazyflie12/odom 100 odometry $SERVINING_IP 201" C-m
+tmux send-keys -t 1 "sleep 1; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_server_py.py /pixy/vicon/demo_crazyflie12/demo_crazyflie12/odom 100 odometry $SERVINING_IP 31404" C-m
 
 # NOTES:
 # For the client script, First argument: topic_to_subscribe, 
@@ -36,10 +36,10 @@ tmux send-keys -t 1 "sleep 1; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp
 #                        Third argument: IP to connect to
 #                        Forth argument: Port
 
-tmux send-keys -t 0 "sleep 2; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_sub_client.py /demo_crazyflie12/cmd_vel velocities $TARGET_IP 31349" C-m
+tmux send-keys -t 3 "sleep 3; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_sub_client.py /demo_crazyflie12/cmd_vel velocities $TARGET_IP 31349" C-m
 
 # for reading the latencies
-tmux send-keys -t 1 "sleep 1; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_server_py_cmd.py /old_cmd_vel 30 old_cmd_node $SERVINING_IP 203" C-m
+tmux send-keys -t 2 "sleep 1; export ROS_IP=127.0.0.1; cd $TEMP_DIR; python3 udp_server_py_cmd.py /old_cmd_vel 30 old_cmd_node $SERVINING_IP 31182" C-m
 
 
 
